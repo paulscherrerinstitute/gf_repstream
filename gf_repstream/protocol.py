59a2352878d2bfc8b26962208c26094524acaf34
lhdamiani <lhdamiani@gmail.com>
2021-09-07 12:19:34 +0200

    ctype structure header.

---
+++ gf_repstream/protocol.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from _ctypes import Structure
+from ctypes import c_uint64, c_uint16
+
+
+class TestMetadata(Structure):
+    _pack_ = 1
+    _fields_ = [
+                ("frame", c_uint64),
+                ("htype", c_uint64),
+                ("tag", c_uint64),
+                ("source", c_uint64),
+                ("shape", c_uint64),
+                ("type", c_uint64),
+                ("endianess", c_uint64)]
+
+    def as_dict(self):
+        return dict((f, getattr(self, f)) for f, _ in self._fields_)
