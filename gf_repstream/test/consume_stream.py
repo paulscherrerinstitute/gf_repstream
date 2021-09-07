16bc7cec95a139b032b9620faef01726ab35df33
lhdamiani <lhdamiani@gmail.com>
2021-09-07 12:24:59 +0200

    formatting

--- gf_repstream/test/consume_stream.py
+++ gf_repstream/test/consume_stream.py
@@ -6,26 +6,26 @@ import zmq
 
 from gf_repstream.protocol import TestMetadata
 
+
 def main(backend):
     # Socket to talk to server
     context = zmq.Context(io_threads=1)
-    if mode.upper() == 'SUB':
+    if mode.upper() == "SUB":
         socket = context.socket(zmq.SUB)
         socket.setsockopt_string(zmq.SUBSCRIBE, u"")
-    elif mode.upper() == 'PULL':
+    elif mode.upper() == "PULL":
         socket = context.socket(zmq.PULL)
     else:
-        raise RuntimeError(
-            "Mode not recognized (SUB or PULL). Halting executing..."
-        )
+        raise RuntimeError("Mode not recognized (SUB or PULL). Halting executing...")
     socket.connect(backend)
 
     total_recvs = 0
     while True:
         data = socket.recv_multipart()
         metadata = TestMetadata.from_buffer_copy(data[0]).as_dict()
-        print(metadata,total_recvs)
+        print(metadata, total_recvs)
         total_recvs += 1
+
 
 if __name__ == "__main__":
     backend = sys.argv[1]
--- gf_repstream/test/fake_stream.py
+++ gf_repstream/test/fake_stream.py
@@ -8,6 +8,7 @@ from ctypes import c_uint64, c_uint16
 
 from gf_repstream.protocol import TestMetadata
 
+
 def main(out_address):
     context = zmq.Context()
     zmq_socket = context.socket(zmq.PUB)
@@ -18,10 +19,13 @@ def main(out_address):
     for num in range(1000):
         data_message = bytearray(random.getrandbits(8) for _ in range(1024))
         # zmq_socket.send_multipart([json.dumps(metadata).encode("utf-8"), data_message])
-        zmq_socket.send_multipart([bytes(TestMetadata(num, 1, 0, 0, 760864, 2, 0)), data_message])
+        zmq_socket.send_multipart(
+            [bytes(TestMetadata(num, 1, 0, 0, 760864, 2, 0)), data_message]
+        )
         time.sleep(0.01)
     time.sleep(1)
     zmq_socket.close()
+
 
 if __name__ == "__main__":
     out_address = sys.argv[1]
