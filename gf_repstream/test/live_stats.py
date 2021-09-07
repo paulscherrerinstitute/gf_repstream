9b418b42221cb8e5e1398e6738e02969d8c6cbd2
lhdamiani <lhdamiani@gmail.com>
2021-09-07 10:24:46 +0200

    binary metadata and cython header. zmq pattern via argument

--- gf_repstream/test/live_stats.py
+++ gf_repstream/test/live_stats.py
@@ -1,23 +1,34 @@
+#!/usr/bin/env python
 import json
 import sys
 import time
 import zmq
 
+from gf_repstream.protocol import TestMetadata
+
 def main(backend):
     # Socket to talk to server
     context = zmq.Context(io_threads=1)
-    socket = context.socket(zmq.SUB)
+    if mode.upper() == 'SUB':
+        socket = context.socket(zmq.SUB)
+        socket.setsockopt_string(zmq.SUBSCRIBE, u"")
+    elif mode.upper() == 'PULL':
+        socket = context.socket(zmq.PULL)
+    else:
+        raise RuntimeError(
+            "Mode not recognized (SUB or PULL). Halting executing..."
+        )
     socket.connect(backend)
-    socket.setsockopt_string(zmq.SUBSCRIBE, u"")
+    
     # Process 5 updates
     total_recvs = 0
     while True:
         data = socket.recv_multipart()
-        metadata = json.loads(data[0].decode())
-        print(metadata)
+        metadata = TestMetadata.from_buffer_copy(data[0]).as_dict()
+        print(metadata,total_recvs)
         total_recvs += 1
-        print("total recvs", total_recvs)
 
 if __name__ == "__main__":
     backend = sys.argv[1]
+    mode = sys.argv[2]
     main(backend)
