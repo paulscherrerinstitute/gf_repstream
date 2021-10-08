b23b05225b4d08e6a6e2fd99188a5cd53da83cb4
Leonardo Hax <lhdamiani@gmail.com>
2021-10-08 20:05:04 +0200

    conda setup fix

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import re
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("gf_repstream/__init__.py") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
@@ -11,6 +11,6 @@ setup(
     author="Leonardo Hax Damiani",
     author_email="leonardo.hax@psi.ch",
     description="Gigafrost stream repeater",
-    packages=find_packages(),
+    packages=['gf_repstream'],
     license="GNU GPLv3",
 )
